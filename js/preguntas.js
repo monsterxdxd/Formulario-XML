419b4476ff7fe33d663a752807e5b758b3a2f4e7
Mariano <mariano.palliser@gmail.com>
2017-02-21 21:08:49 +0100

    Update preguntas.js

--- js/preguntas.js
+++ js/preguntas.js
@@ -32,7 +32,7 @@ function gestionarXml(dadesXml){
 		
 function ponerDatosSelectHtml(title,opt){
 	document.getElementById("firstQuestion").innerHTML=title;
-	var select = document.getElementsByTagName("select")[0];
+	var select = document.getElementsByTagName("select");
 	for (i = 0; i < opt.length; i++) { 
 		var option = document.createElement("option");
 		option.text = opt[i];
